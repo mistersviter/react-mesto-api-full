d770a0883aeeb023a65e731d9865f24ebc393812
Tarabrin Evgeniy <mistersviter@gmail.com>
2022-09-10 14:36:26 +0400

    fix: fix errorLogger after first review

--- backend/app.js
+++ backend/app.js
@@ -66,10 +66,6 @@ app.use(auth);
 app.use('/users', require('./routes/users'));
 app.use('/cards', require('./routes/cards'));
 
-app.get('/signout', (req, res) => {
-  res.clearCookie('jwt').send({ message: 'Выход' });
-});
-
 app.use(errorLogger);
 
 app.use((req, res, next) => {
